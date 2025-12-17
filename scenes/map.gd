00b62d10daa0ccbe42345b0ac8dcae7679b5157e
loucks.dev <parkerloucks74@gmail.com>
2025-12-16 18:12:42 -0600

    Grid math and pathfinding function essentials

---
+++ scenes/map.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+@onready var tilemap: TileMapLayer = $TileMapLayer
+
+func world_to_tile(world_pos: Vector2) -> Vector2i:
+	return tilemap.local_to_map(tilemap.to_local(world_pos))
+
+func tile_to_world(tile_pos: Vector2i) -> Vector2:
+	return tilemap.to_global(tilemap.map_to_local(tile_pos))
+
+# DEBUG
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		var world_pos = get_global_mouse_position()
+		var tile = world_to_tile(world_pos)
+		print("Clicked tile:", tile)
+
+func is_tile_walkable(tile_pos: Vector2i) -> bool:
+	var tile_data = tilemap.get_cell_tile_data(tile_pos)
+	if tile_data == null:
+		return false
+	return tile_data.get_custom_data("walkable") == true
+
+# AStarGrid2D 
+var astar := AStarGrid2D.new()
+
+func _ready():
+	astar.cell_size = tilemap.tile_set.tile_size # for visualization later
+	astar.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
+
+	var used_rect = tilemap.get_used_rect()
+	astar.region = used_rect
+	astar.update()
+
+	for x in range(used_rect.position.x, used_rect.position.x + used_rect.size.x):
+		for y in range(used_rect.position.y, used_rect.position.y + used_rect.size.y):
+			var pos = Vector2i(x, y)
+			astar.set_point_solid(pos, not is_tile_walkable(pos))
+
+func find_path(from_tile: Vector2i, to_tile: Vector2i) -> Array[Vector2i]:
+	if astar.is_point_solid(to_tile):
+		return []
+	return astar.get_id_path(from_tile, to_tile)
