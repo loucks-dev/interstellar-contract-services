bd7877ad50dc10d1e2234993efc9a3354e5627ee
taxisnacks <parkerloucks74@gmail.com>
2025-12-28 13:05:41 -0600

    Formatting changes.

--- scenes/unit.gd
+++ scenes/unit.gd
@@ -71,8 +71,10 @@ func spend_movement(cost: int) -> void:
 	print("Movement taken, new AP:", action_points)
 
 func execute_attack(attacker: Unit, target: Unit):
+	# var hit_chance = (just for reference; need to research typical implementations of this)
 	var unit_manager = get_tree().get_first_node_in_group("unit_manager")
 	print(attacker.name, "attacks", target.name)
+	
 	attacker.action_points -= 1
 	target.take_damage(3)
 	unit_manager.deselect_active_unit()
